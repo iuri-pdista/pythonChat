3c2afee2fb21f9287190b02dc3b7448b468dceb3
iuri-pdista <correaiuri86@gmail.com>
2020-07-22 15:37:31 -0300

    Improved aplication messages/alerts  display

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
 import colorama 
 
-userName = input("Enter your username(it will be visible to all in the chat)")
+userName = input(">>>Enter your username(it will be visible to all in the chat): ")
 try:
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.connect((socket.gethostname(),21000))
 except: 
-    print(Fore.red + 'Could not connect with the server')
+    print(Fore.red + '>>>Could not connect with the server')
 
 welcomeMessage = serverSocket.recv(100)
 print(welcomeMessage.decode("utf-8"))
@@ -17,8 +17,8 @@ try:
         if len(recvMessage) != 0:
             print(recvMessage.decode("utf-8"))
         else:
-            print("No message was recieved")
+            print(">>>No message was recieved")
         sendMessage = input()
         serverSocekt.send(bytes(sendMessage,"utf-8"))
 except:
-    print("Lost connection with server")
+    print(">>>Lost connection with server")
