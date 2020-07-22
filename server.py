6ba2e2cf9b567881feb56ba00d1e0eb3c657b73d
iuri-pdista <correaiuri86@gmail.com>
2020-07-22 14:54:28 -0300

    first commit :tada::tada:

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+try:
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((socket.gethostname(),21000))
+    serverSocket.listen(3)
+except:
+    print("Could not establish the server")
+
+while True:
+    clientsocket, address = serverSocket.accept()
+    print(f"Connection from {address} has been established!")
+    clientsocket.send(bytes("W&LC0M3 T0 T#& C#H4T", "utf-8"))
+    clientName = clientsocket.recv(15)
+    print(f"The address: {address} joined the server as: {clientName}")
+    clientsocket.close()
+serverSocket.close()
+
+
