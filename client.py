56e5bb1c831c5d0c83f1d1ffef5f26d0a0c257eb
iuri-pdista <correaiuri86@gmail.com>
2020-07-22 15:17:30 -0300

    Improved client experience

--- client.py
+++ client.py
@@ -1,3 +1,13 @@
 import socket
+import colorama 
 
+userName = input("Enter your username(it will be visible to all in the chat)")
+try:
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.connect((socket.gethostname(),21000))
+except: 
+    print(Fore.red + 'Could not connect with the server')
 
+welcomeMessage = serverSocket.recv(100)
+print(welcomeMessage.decode("utf-8"))
+serverSocket.send(bytes(userName, "utf-8"))
